256edf5310ebd46e644855c2365c392eaba92d4b
paul-frdev <pavelzadyrko@gmail.com>
2022-02-17 17:32:14 +0200

    add: burgerMenu for mobile

---
+++ src/js/modules/burger.js
@@ -0,0 +1,27 @@
+const burger = (menuSelector, burgerSelector) => {
+
+    const menuElem = document.querySelector(menuSelector),
+        burgerElem = document.querySelector(burgerSelector);
+
+    menuElem.style.display = 'none';
+
+    burgerElem.addEventListener('click', () => {
+        if (menuElem.style.display === 'none' && window.screen.availWidth <= 993) {
+            menuElem.style.display = 'block';
+            menuElem.classList.add('fadeIn', 'animated')
+        } else {
+            menuElem.style.display = 'none';
+            menuElem.classList.remove('fadeIn', 'animated')
+        }
+    });
+
+
+    window.addEventListener('resize', () => {
+        if (window.screen.availWidth > 992) {
+            menuElem.style.display = 'none';
+        }
+    })
+
+}
+
+export default burger;
